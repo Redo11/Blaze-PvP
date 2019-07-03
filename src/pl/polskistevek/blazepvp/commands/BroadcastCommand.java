fa26d6c7f1fe3260f42b6844c579cf4ec62e39cc
jambosmaha <jambosmaha@gmail.com>
2019-07-03 21:12:11 +0200

    Dodanie Brodcast

---
+++ src/pl/polskistevek/blazepvp/commands/BroadcastCommand.java
@@ -0,0 +1,36 @@
+package pl.polskistevek.blazepvp.commands;
+                import org.bukkit.Bukkit;
+                import org.bukkit.command.Command;
+                import org.bukkit.command.CommandExecutor;
+                import org.bukkit.command.CommandSender;
+                import org.bukkit.entity.Player;
+
+public class BroadcastCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        String permission = Config.getString("permission") + "bc";
+        if (sender.hasPermission(permission)){
+            if (args.length == 0){
+                sender.sendMessage("Podaj wiadomosc!");
+                return false;
+            }
+            else{
+                final StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < args.length; ++i) {
+                    sb.append(args[i]).append(" ");
+                }
+                final String message = sb.toString();
+                Bukkit.broadcastMessage("");
+                Bukkit.broadcastMessage(ChatUtil.fix("&2&lOGLOSZENIE &8>> &6" + message));
+                Bukkit.broadcastMessage("");
+                for (Player u : Bukkit.getOnlinePlayers()){
+                    TitleAPI.sendTitle(u, 10, 150, 10, ChatUtil.fix("&8[&4OGLOSZENIE&8]"), ChatUtil.fix("&6" +message));
+                }
+            }
+            return false;
+        }else{
+            Utils.noPermission((Player) sender, permission);
+        }
+        return false;
+    }
+}
